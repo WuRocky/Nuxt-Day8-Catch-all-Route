a51be933ec0b80fa9f3c04e91b5ab509c340ec42
Rocky Wu <rocky_wu@everrich.com.tw>
2024-11-15 10:17:23 +0800

    initialization Day8

--- pages/room.vue
+++ pages/room.vue
@@ -1,7 +1,21 @@
-<script setup></script>
+<script setup>
+// const roomsList = ref([]);
+// 使用 fetch 或 axios 串接 前台房型 API ( GET )
+// apiUrl : https://nuxr3.zeabur.app/api/v1/rooms
+// response 回傳後，將資料寫入 roomsList 變數
+// 使用 roomsList 變數在下方 template 渲染列表
+
+</script>
 
 <template>
+  <h1>房型頁面</h1>
   <NuxtPage />
 </template>
 
-<style scoped></style>
+<style scoped>
+.card-img-top {
+  object-fit: cover;
+  height: 200px;
+  max-width: 100%;
+}
+</style>
